c08650a2851260110dffe79b6f499e1e4e7ec238
Drew Gardino <69988018+DGardino09@users.noreply.github.com>
2022-08-22 13:21:01 -0500

    Getting Web Page Running and Creating Access Tokens and Redirect Tokens
    I created the web page using flask in Python and we able to get the web page running by entering in my Client ID and Client Secret. For now the web pages only have text on them. The next step is to actually pull info from the Spotify API to display on the screen.

--- Spotify API Call.py
+++ Spotify API Call.py
@@ -1,8 +1,42 @@
+from flask import Flask, url_for, session, redirect, request
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
-class Spotify_API_Call():
-    response = requests.get("https://api.spotify.com")
-    print(response.status_code)
+app = Flask(__name__)
+app.secret_key = "rkjalnfncion"
+app.config['SESSION_COOKIE_NAME'] = "Drew Cookie"
+TOKEN_KEY = "token_info"
 
-if __name__ == "__main__":
-    SpotifyAPI = Spotify_API_Call();
+@app.route('/')
+def login():
+    spotifyAuth = createSpotifyOAuth()
+    auth_url = SpotifyOAuth.get_authorize_url(spotifyAuth)
+    return redirect(auth_url)
+
+@app.route('/redirect')
+def redirectPage():
+    spotifyAuth = createSpotifyOAuth()
+    session.clear()
+    code = request.args.get("code")
+    token_info = spotifyAuth.get_access_token(code)
+    session[TOKEN_KEY] = token_info
+    return redirect(url_for('getTopArtists', _external=True))
+
+@app.route('/getTopArtists')
+def getTopArtists():
+    return "Top Artists"
+
+def createSpotifyOAuth():
+    return SpotifyOAuth(client_id="CLIENT_ID", client_secret="CLIENT_SECRET",
+                        redirect_uri=url_for('redirectPage', _external=True), scope="user-library-read")
+
+#TODO: Create refresh token
+#TODO: Create a playlist
+#TODO: Add songs to a playlist
+#TODO: Figure out how to pull similar artists based on the users most listened to songs and artists
+#TODO: Use JavaScript to make webpage look nice
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
