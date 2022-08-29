9c79c1737a7f8ac7142e20892011667682721134
Drew Gardino <69988018+DGardino09@users.noreply.github.com>
2022-08-29 14:08:23 -0500

    Fixing Access Token and Building Refresh Token
    I fixed my access token once I learned more about how it worked and built a refresh token. The refresh token helps in case the access token expires.

--- Spotify API Call.py
+++ Spotify API Call.py
@@ -1,4 +1,8 @@
+import time
+
 from flask import Flask, url_for, session, redirect, request
+import json
+import pandas as pd
 import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -8,6 +12,7 @@ app = Flask(__name__)
 app.secret_key = "rkjalnfncion"
 app.config['SESSION_COOKIE_NAME'] = "Drew Cookie"
 TOKEN_KEY = "token_info"
+
 clientID = "ClientID"
 clientSecret = "ClientSecret"
 
@@ -28,17 +33,45 @@ def redirectPage():
     session[TOKEN_KEY] = token_info
     return redirect(url_for('getRecentlyPlayed', _external=True))
 
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect(url_for('login', _external=False))
+
 @app.route('/getRecentlyPlayed')
 def getRecentlyPlayed():
-    sp = spotipy.Spotify(client_credentials_manager=clientCredentials)
+    session['token_info'], authorized = getToken()
+    session.modified = True
+    if not authorized:
+        return url_for('login', _external=False)
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
     currentUserRecentlyPlayed = sp.current_user_recently_played(limit=50, after=None, before=None)
-    return currentUserRecentlyPlayed
+    return str(currentUserRecentlyPlayed)
+
+def getToken():
+    validToken = False
+    tokenInfo = session.get("token_info", {})
+    
+    if not (session.get("token_info", False)):
+        validToken = False
+        return tokenInfo, validToken
+
+    currentTime = int(time.time())
+    tokenExpired = session.get("token_info").get("expires_at") - currentTime < 60
+
+    if (tokenExpired):
+        spOAuth = createSpotifyOAuth()
+        tokenInfo = spOAuth.refresh_access_token(session.get("token_info").get("refresh_token"))
+
+    validToken = True
+    return tokenInfo, validToken
 
 def createSpotifyOAuth():
     return SpotifyOAuth(client_id=clientID, client_secret=clientSecret,
                         redirect_uri=url_for('redirectPage', _external=True), scope="user-read-recently-played")
 
-#TODO: Create refresh token
+#TODO: Use Pandas to get DataFrame and specify what data to pull
 #TODO: Create a playlist
 #TODO: Add songs to a playlist
 #TODO: Figure out how to pull similar artists based on the users most listened to songs and artists
