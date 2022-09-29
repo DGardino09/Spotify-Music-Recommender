377022253995cba7f2bfce86804cc5c99ab22e92
Drew Gardino <69988018+DGardino09@users.noreply.github.com>
2022-09-28 22:35:52 -0500

    Completing Project
    I finished creating and building all methods needed for this project. I then had to modify some of those methods when building out the driver class to make sure that all of the correct information was being returned. Finally I tested creating and modifying the playlist that the algorithm created and verified that everything worked and all of the correct songs were added to my Spotify account.

--- Spotify API Call.py
+++ Spotify API Call.py
@@ -1,29 +1,30 @@
+import random
+
 from flask import Flask, url_for, session, redirect, request
 import json
 import pandas as pd
+import random
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.oauth2 import SpotifyClientCredentials
 import time
 
-#TODO: Create a playlist
 #TODO: Add songs to a playlist
-#TODO: Figure out how to pull similar artists based on the users most listened to songs and artists
-#TODO: Use JavaScript to make webpage look nice
 
 app = Flask(__name__)
 app.secret_key = "rkjalnfncion"
-app.config['SESSION_COOKIE_NAME'] = "Drew Cookie"
+app.config['SESSION_COOKIE_NAME'] = "Cookie"
 TOKEN_KEY = "token_info"
-
-clientID = "ClientID"
-clientSecret = "ClientSecret"
+clientID = "d3ec161021af4eaf959491dd6757c7e1"
+clientSecret = "3a8e0bf588174082bcc2961be2afaba5"
+# clientID = "ClientID"
+# clientSecret = "ClientSecret"
 
 clientCredentials = SpotifyClientCredentials(client_id=clientID, client_secret=clientSecret)
 
 def createSpotifyOAuth():
     return SpotifyOAuth(client_id=clientID, client_secret=clientSecret,
-                        redirect_uri=url_for('redirectPage', _external=True), scope="user-top-read user-read-recently-played")
+                        redirect_uri=url_for('redirectPage', _external=True), scope="user-top-read user-read-recently-played playlist-modify-public playlist-read-private")
 
 def getToken():
     validToken = False
@@ -56,7 +57,7 @@ def redirectPage():
     code = request.args.get("code")
     token_info = spotifyAuth.get_access_token(code)
     session[TOKEN_KEY] = token_info
-    return redirect(url_for('getUserTopArtists', _external=True))
+    return redirect(url_for('homePage', _external=True))
 
 @app.route('/logout')
 def logout():
@@ -80,9 +81,9 @@ def getRecentlyPlayed():
             results.append(trackValue)
         if (idx < 50):
             break
-    dataframe = pd.DataFrame(results, columns=['Track Names'])
-    dataframe.to_csv('recentlyPlayed.csv', index=True)
-    return "Recently Played songs compiled"
+    # dataframe = pd.DataFrame(results, columns=['Track Names'])
+    # dataframe.to_('recentlyPlayed.csv', index=True)
+    return results
 
 @app.route('/getUserTopArtists')
 def getUserTopArtists():
@@ -92,20 +93,25 @@ def getUserTopArtists():
         return url_for('login', _external=False)
     sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
     results = []
+    artistIDList = []
     limit = 20
+
+    # topArtists = sp.current_user_top_artists(limit=limit, offset=0, time_range="medium_term")
+    # return topArtists
+
     while True:
-        topArtists = sp.current_user_top_artists(limit=20, offset=0, time_range="medium_term")["items"]
+        topArtists = sp.current_user_top_artists(limit=limit, offset=0, time_range="medium_term")["items"]
         for idx, item in enumerate(topArtists):
             artist = item["name"]
             artistID = item["uri"]
-            relatedArtists = getRelatedArtists(artist, artistID)
-            getArtistTopTracks(artistID)
+            artistTopTracks = getArtistTopTracks(artistID)
             results.append(artist)
+            artistIDList.append(artistID)
         if (idx < 50):
             break
-    dataframe = pd.DataFrame(results, columns=["Top " + str(limit) + " Artists"])
-    dataframe.to_csv('Top Artists.csv', index=True)
-    return "Top Artists Complied"
+    # dataframe = pd.DataFrame(results, columns=["Top " + str(limit) + " Artists"])
+    # dataframe.to_csv('Top Artists.csv', index=True)
+    return results, artistTopTracks, artistIDList
 
 @app.route('/getUserTopTracks')
 def getUserTopTracks():
@@ -121,18 +127,17 @@ def getUserTopTracks():
         count = count + 1
         topTracks = sp.current_user_top_tracks(limit=50, offset=offset, time_range="medium_term")["items"]
         for idx, item in enumerate(topTracks):
-            track = item["name"]
+            track = item["uri"]
             artist = item["album"]
             trackValue = track + " : " + artist["artists"][0]["name"]
-            results.append(trackValue)
+            results.append(track)
         if (len(topTracks) < 50):
             break
-    dataframe = pd.DataFrame(results, columns=['Track Names'])
-    dataframe.to_csv('TopTracks.csv', index=True)
-    return "Top Played Songs compiled"
+    # dataframe = pd.DataFrame(results, columns=['Track Names'])
+    # dataframe.to_csv('TopTracks.csv', index=True)
+    return results
 
-
-def getRelatedArtists(artistName, artistID):
+def getRelatedArtists(artistID):
     session['token_info'], authorized = getToken()
     session.modified = True
     if not authorized:
@@ -144,22 +149,141 @@ def getRelatedArtists(artistName, artistID):
         for idx, item in enumerate(relatedArtists):
             artist = item["name"]
             relatedArtistID = item["uri"]
-            getArtistTopTracks(relatedArtistID)
-            results.append(artist)
+            #results.append(artist)
+            results.append(relatedArtistID)
         if (idx < 50):
             break
-    dataframe = pd.DataFrame(results, columns=["Related Artists"])
-    dataframe.to_csv("Artists Related to " + artistName + ".csv", index=True)
-    return dataframe
+    # dataframe = pd.DataFrame(results, columns=["Related Artists"])
+    # dataframe.to_csv("Artists Related to " + artistName + ".csv", index=True)
+    return results
 
+@app.route("/getArtistTopTracks")
 def getArtistTopTracks(artistID):
-    return "some songs"
+    session['token_info'], authorized = getToken()
+    session.modified = True
+    if not authorized:
+        return url_for('login', _external=False)
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    results = []
 
-def createPlaylist():
-    return "blank playlist"
+    # artistTopTracks = sp.artist_top_tracks(artist_id="11gWrKZMBsGQWmobv3oNfW")
+    # return artistTopTracks
 
-def modifyPlaylist():
-    return "recommended playlist"
+    while True:
+        artistsTopTracks = sp.artist_top_tracks(artist_id=artistID)["tracks"]
+        for idx, item in enumerate(artistsTopTracks):
+            track = item["uri"]
+            artist = item["artists"][0]["name"]
+            trackValue = track + " : " + artist
+            results.append(track)
+        if (idx < 50):
+            break
+    # dataframe = pd.DataFrame(results, columns=["Related Artists"])
+    # dataframe.to_csv("Top Tracks for " + str(artist) + ".csv", index=True)
+    return results
+
+def createPlaylist(name):
+    session['token_info'], authorized = getToken()
+    session.modified = True
+    if not authorized:
+        return url_for('login', _external=False)
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    playlist = sp.user_playlist_create(user="dgardino-us", name=name, description="This is a playlist generated by my Spotify recommendation algorithm.")
+    return playlist
+
+def modifyPlaylist(name, playlist):
+    session['token_info'], authorized = getToken()
+    session.modified = True
+    if not authorized:
+        return url_for('login', _external=False)
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    sp.playlist_add_items(playlist_id=name, items=playlist)
+    return "playlist done being modified."
+
+@app.route("/playlistInfo")
+def playlistInfo():
+    session['token_info'], authorized = getToken()
+    session.modified = True
+    if not authorized:
+        return url_for('login', _external=False)
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    playlist = sp.current_user_playlists()["items"][0]["id"]
+    return playlist
+
+def randomizer(listLength):
+    low = 0
+    high = listLength - 1
+    randomPos = random.randint(a=low, b=high)
+    return randomPos
+
+def getUser():
+    session['token_info'], authorized = getToken()
+    session.modified = True
+    if not authorized:
+        return url_for('login', _external=False)
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+    user = sp.current_user()
+    return user
+
+@app.route('/home')
+def homePage():
+    playlistName = input("What do you want to name this playlist? ")
+    createPlaylist(playlistName)
+    recommendedPlaylist = []
+    unsortedPlaylist = []
+    topTracks = getUserTopTracks()
+    userTopArtists = getUserTopArtists()
+    artistIDList = userTopArtists[2]
+    topArtistsTopTracks = userTopArtists[2]
+    relatedArtistLimit = 50
+    topTracksLimit = 20
+    topArtistsLimit = 30
+    count = 0
+
+    session['token_info'], authorized = getToken()
+    session.modified = True
+    if not authorized:
+        return url_for('login', _external=False)
+    sp = spotipy.Spotify(auth=session.get('token_info').get('access_token'))
+
+    for i in range(len(artistIDList)):
+        artistID = artistIDList[i]
+        relatedArtists = getRelatedArtists(artistID)
+        for i in range(2):
+            randInt = randomizer(len(relatedArtists))
+            relatedArtistID = relatedArtists[randInt]
+            relatedArtistsTopTracks = getArtistTopTracks(relatedArtistID)
+            relatedArtistsTopTracksLength = len(relatedArtistsTopTracks)
+            for i in range(relatedArtistsTopTracksLength):
+                temp = relatedArtistsTopTracks.pop()
+                unsortedPlaylist.append(temp)
+    for i in range(relatedArtistLimit):
+        randInt = randomizer(len(unsortedPlaylist))
+        track = unsortedPlaylist[randInt]
+        recommendedPlaylist.append(track)
+    unsortedPlaylist.clear()
+    for i in range(len(topTracks)):
+        temp = topTracks.pop()
+        unsortedPlaylist.append(temp)
+    for i in range(topTracksLimit):
+        randInt = randomizer(len(unsortedPlaylist))
+        track = unsortedPlaylist[randInt]
+        recommendedPlaylist.append(track)
+    unsortedPlaylist.clear()
+    for i in range(len(topArtistsTopTracks)):
+        temp = topArtistsTopTracks.pop()
+        tempTopTracks = getArtistTopTracks(temp)
+        for i in range(len(tempTopTracks)):
+            track = tempTopTracks.pop()
+            unsortedPlaylist.append(track)
+    for i in range(topArtistsLimit):
+        randInt = randomizer(len(unsortedPlaylist))
+        track = unsortedPlaylist[randInt]
+        recommendedPlaylist.append(track)
+    unsortedPlaylist.clear()
+    playlistID = playlistInfo()
+    modifyPlaylist(playlistID, recommendedPlaylist)
+    return "Recommended Playlist has been created!"
 
 if __name__ == '__main__':
 	app.run(debug=True)
